5ef80c6d6e79e7edd26294a3ef0552ba1b67ec9b
Indhumathi C <indhumathi93428@gmail.com>
2026-02-05 07:10:51 +0000

    LeetCode Synchronization - Palindrome Number (Java)

--- README.md
+++ README.md
@@ -6,4 +6,5 @@
 
 | # | Title | Difficulty | Skills |
 |---| ----- | ---------- | ------ |
+| 0009 | [Palindrome Number](https://leetcode.com/problems/palindrome-number) | Easy | `Math` |
 | 0104 | [Maximum Depth of Binary Tree](https://leetcode.com/problems/maximum-depth-of-binary-tree) | Easy | `Binary Tree` `Breadth-First Search` `Depth-First Search` `Tree` |
---
+++ problems/0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }int rev=0;
+        int org=x;
+        while(x>0){
+            int rem=x%10;
+             rev=rev*10+rem;
+            x=x/10;
+        }
+        if(org==rev){
+            return true;
+        }else{
+            return false;
+        }
+}}
