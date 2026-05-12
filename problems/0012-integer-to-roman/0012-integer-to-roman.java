96184105a9af771d243af3101add66da04817cf4
Indhumathi C <indhumathi93428@gmail.com>
2026-05-12 14:20:33 +0000

    LeetCode Synchronization - Integer to Roman (Java)

---
+++ problems/0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String intToRoman(int num) {
+        String[] roman= {"M","CM","D","CD",
+"C","XC","L","XL",
+"X","IX","V","IV","I"};
+        int[] value={1000,900,500,400,
+100,90,50,40,
+10,9,5,4,1}; String res="";
+
+        for(int i=0;i<roman.length;i++){
+            while(num>=value[i]){
+               num=num-value[i];
+               res+=roman[i];       
+        }
+        }return res;
+ 
+    }
+}
