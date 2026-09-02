23cd2d8ea861730773fe5e26c822dd73451605c5
Indhumathi C <indhumathi93428@gmail.com>
2026-09-02 06:31:36 +0000

    LeetCode Synchronization - Ransom Note (Java)

---
+++ problems/0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int freq[]=new int[256];
+        for(int i=0;i<magazine.length();i++){
+            freq[magazine.charAt(i)]++;
+        }
+        for(int i=0;i<ransomNote.length();i++){
+            freq[ransomNote.charAt(i)]--;
+        }
+       for(int j:freq){
+        if(j<0){
+            return false;
+        }
+       }return true;
+    }
+}
